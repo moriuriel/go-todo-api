43950732650aadcbf35d3a0f5f42faa91dc2f6e2
Uriel Mori Vanso <moriuriel@gmail.com>
2021-12-23 15:21:15 -0300

    chore: adding mux

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func createRouter() *mux.Router {
+	router := mux.NewRouter()
+
+	return router
+}
 
 func main() {
+
+	router := createRouter()
+	log.Fatal(http.ListenAndServe(":5000", router))
+
 	fmt.Println("Server is starting on port")
 }
